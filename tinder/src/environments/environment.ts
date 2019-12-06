93c68b36dbd886e95d0debe7839ad5a228942744
Moritz Ralph Dück <moritz-ralph.dueck@stud.uni-bamberg.de>
2019-12-06 23:23:41 +0100

    environment

--- tinder/src/environments/environment.ts
+++ tinder/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // rest API config
+  API_HOST: 'http://',
+  API_PORT: 8080,
 };
 
 /*
